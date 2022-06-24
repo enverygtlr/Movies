c7b83b4849fbf3378000a60c8d9a781f450fb571
enverygtlr <36340901+enverygtlr@users.noreply.github.com>
2022-06-24 13:27:27 +0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Enver Yigitler on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
