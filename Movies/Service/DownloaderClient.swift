0b7da56d1178d3d26722438e9765b58740dbee13
Berat Yavuz <beratttyavuzz@gmail.com>
2022-06-27 11:37:35 +0300

    DownloaderClient created

--- Movies/Service/DownloaderClient.swift
+++ Movies/Service/DownloaderClient.swift
@@ -12,9 +12,18 @@ class DownloaderCLient {
     // aranacak film ve bittikten sonra ne olacağını main threadi bloklamamak için escape succes ve failure verdik
     func filmleriIndir(search:String, completion: @escaping (Result<[Film]?,DownloaderError>) -> Void ) {
         
-        guard let url = URL (string: "http://www.omdbapi.com/?i=tt3896198&apikey=f82a83aa") else {
+        guard let url = URL (string: "http://www.omdbapi.com/?s=\(search)&apikey=f82a83aa") else {
             return completion(.failure(.yanlisUrl))
         }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data,  error == nil else {
+                return  completion(.failure(.veriGelmedi))
+            }
+            guard let filmCevabi =  try? JSONDecoder().decode(GelenFilmler.self, from: data) else {
+                return completion(.failure(.veriIslenmedi))
+            }
+            completion(.success(filmCevabi.filmler))
+        }.resume()
         
     }
 }
